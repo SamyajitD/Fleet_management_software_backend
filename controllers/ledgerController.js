6f4e3441b76619150b2e32db044f20af296be031
SamyajitD <samyajitd@gmail.com>
2025-01-15 03:28:38 +0530

    fix ledger

--- controllers/ledgerController.js
+++ controllers/ledgerController.js
@@ -14,7 +14,7 @@ module.exports.newLedger = async(req, res) => {
         });
 
         await newLedger.save();
-        return res.status(201).json({ message: "Successfully created ledger entry", ledger: newLedger });
+        return res.status(200).json({ message: "Successfully created ledger entry", ledger: newLedger });
     } catch (err) {
         res.status(500).json({ message: "Failed to create a new driver", err });
     }
